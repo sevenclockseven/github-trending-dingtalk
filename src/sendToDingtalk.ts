8b7fd13e7038e6fbd5a9eed919fc47089db54303
weihc <weihc@trgroup.cn>
2025-11-27 11:15:11 +0800

    再次修改 sendToDingtalk 以适配新的 formatProject 函数

--- src/sendToDingtalk.ts
+++ src/sendToDingtalk.ts
@@ -1,14 +1,14 @@
 import axios from 'axios';
-import { formatProject } from './utils/format';
+import { formatProject, Project } from './utils/format';
 
 const DINGTALK_WEBHOOK_URL = process.env.DINGTALK_WEBHOOK_URL;
 
-export const sendToDingtalk = async (projects: any[]) => {
+export const sendToDingtalk = async (projects: Project[]) => {
     if (!DINGTALK_WEBHOOK_URL) {
         throw new Error('DINGTALK_WEBHOOK_URL is not defined in environment variables');
     }
 
-    const formattedData = formatProjectData(projects);
+    const formattedData = projects.map(formatProject).join('\n\n');
 
     try {
         await axios.post(DINGTALK_WEBHOOK_URL, {
