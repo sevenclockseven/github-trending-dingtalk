e3f9d842ce25732e5d04927ab006b9ebbc7d80af
weihc <weihc@trgroup.cn>
2025-11-27 11:02:24 +0800

    ok

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,16 +1,3 @@
-function formatProjectData(project) {
-    return {
-        title: project.name,
-        url: project.html_url,
-        description: project.description || 'No description available',
-        stars: project.stargazers_count,
-        forks: project.forks_count,
-        language: project.language || 'Not specified',
-        owner: project.owner.login,
-    };
-}
-
-export { formatProjectData };
 export interface Project {
     id?: number | string;
     name: string;
